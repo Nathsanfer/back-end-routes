b7d35074427f1b8ea1fbf59e3ee9e3d74e3aaa00
Nathalia Santos Ferreira <nathalia.s.ferreira9@aluno.senai.br>
2024-08-29 10:53:36 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -8,6 +8,54 @@ const serverPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+    {
+        id: 2,
+        nome: "Raiva",
+        cor: "Vermelho"
+    },
+    {
+        id: 3,
+        nome: "Alegria",
+        cor: "Amarelo"
+    }
+]
+
+const personagens = [
+    {
+        id: 100,
+        nome: "Totoro",
+        studio: "Ghibli",
+        vivo: true
+    },
+    {
+        id: 101,
+        nome: "Princesa Sofia",
+        studio: "Disney",
+        vivo: true
+    },
+    {
+        id: 102,
+        nome: "Rengoku",
+        studio: "Ufotable",
+        vivo: false
+    }
+]
+
+
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send(emocoes)
+})
+
+app.get("/personagens", (req, res) => {
+    return res.status(200).send(personagens)
+})
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, world!"})
 })
